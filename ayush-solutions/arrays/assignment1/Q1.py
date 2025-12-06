cbde7faada5da049cbc19f3c473491144be958b2
Navdeep Kumar <aryaayush9445@gmail.com>
2025-12-06 08:40:59 +0530

    Q.1 Solution

---
+++ ayush-solutions/arrays/assignment1/Q1.py
@@ -0,0 +1,7 @@
+# Q1. Print an Array
+# This program takes an array and prints each element in a single line.
+
+arr = [2, 4, 6, 8] 
+# Looping through each element and print it
+for num in arr:
+    print(num, end=' ')  # Using end=' ' to print elements in one line
