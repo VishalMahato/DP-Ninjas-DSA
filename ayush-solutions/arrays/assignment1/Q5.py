21f2da3751992ef6a61bf213511187a2930d3c16
Navdeep Kumar <aryaayush9445@gmail.com>
2025-12-06 17:24:27 +0530

    Solution Q4 and Q5

--- ayush-solutions/arrays/assignment1/Q5.py
+++ ayush-solutions/arrays/assignment1/Q5.py
@@ -1,8 +1,20 @@
 # Print All 0’s and 1’s Separately
 #  Given an array containing 0s and 1s, print all the 0s first and then all the 1s.
-arr = [1, 0, 1, 0, 0, 1, 1, 0]
-arr.sort()   # array ko sort krega 
+arr = [1, 0, 1, 0, 0, 1]
 
-for item in arr:
-    print(item, end=" ")
-    
+countZero = 0
+countOne = 0
+
+for num in arr:
+    if num == 0:
+        countZero += 1
+    else:
+        countOne += 1
+
+# print 0s first
+for i in range(countZero):
+    print(0, end=" ")
+
+# Then print 1s
+for i in range(countOne):
+    print(1, end=" ")
